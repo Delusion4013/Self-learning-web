8d0c96cf2473eb186c74d32a591d5ed615987419
Omari Thompson-Edwards <marzipant.e@gmail.com>
2020-12-02 12:47:17 +0000

    The websiite now takes grid size params from the json file

--- react-website/self-learning-app/src/App.js
+++ react-website/self-learning-app/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
-import LayoutGrid from './LayoutGrid';
-import currentLayout from './grid-layouts/test.json';
+import LayoutGrid from './LayoutGrid'; 
+import currentLayout from './grid-layouts/test.json'; //Can just omit the file type
 function App() {
   return (
     <div id="container">
@@ -11,7 +11,7 @@ function App() {
 			</h1>
 		</header>
 		{
-			console.log(currentLayout)
+			console.log(currentLayout) //NB: Useful for debugging, you can pretty much just output anything to console and it'll either format it or just convert it to a string
 		}
 		<LayoutGrid layout={currentLayout}>
 		</LayoutGrid>
--- react-website/self-learning-app/src/LayoutGrid.js
+++ react-website/self-learning-app/src/LayoutGrid.js
@@ -4,11 +4,14 @@ import TaskButton from './TaskButton';
 
 
 function LayoutGrid(props) {
-	let elements = props.layout;
+	let elements = props.layout.elements;
+	let gridParams = props.layout.gridParams;
+	let repeatColumns = 	`repeat(${gridParams.w}, 1fr)`;
+	let repeatRows = 	`repeat(${gridParams.h}, 1fr)`;
 	return (
-		<div className="layout-grid">
+		<div className="layout-grid" style={{gridTemplateColumns: repeatColumns, gridTemplateRows: repeatRows}}>
 			{elements.map(e => {
-				return (<TaskButton id={e.id} rect={e.rect}> 
+				return (<TaskButton key={e.id} id={e.id} rect={e.rect}> 
 					{e.content}
 				</TaskButton>)
 			})}
