aa23254ac3fbb80895c47a604517025f0018d580
Shuj Thairu <psyst4@exmail.nottingham.ac.uk>
2020-12-02 12:30:44 +0000

    Prototype: Added enviroment

--- rl-model/rl_model.py
+++ rl-model/rl_model.py
@@ -2,47 +2,59 @@ import sys
 import numpy
 import math
 import random
-
+import random as rand
 import gym
-import gym_game
+import gym_envs
+website_running = true
 #For the prototype rl_model should recive an input of the web configration "State" and the rewards for that state base of activity collector
 #
 #Gets the configration of the website from RL adapter
-def get_state():
-    state = None
-    print("get state")
+def getState():
+    state = env.reset()
     return state
 
 #returns the new state 
 def new_state():
     state = get_state()
-    if rand.unifomrm(0,1)< epsilon:
-        #select a random action from action space
-        #Requires enviromet to be defined
-        #action = env.action_space.sample()
-        print("Explore")
-    else:
-        #Selects best action from action space using q table
-        action = numpy.argmax(q_table[state])
-        print("Exploit")
-        #This returns fuction returns next_state,reward and done
-        env.step(action)
-        #Qlearing eqation 
-        q_table[state][action] = (1 - learning_rate) * q_value + learning_rate + (reward + gamma + best_q)
+    
+#Simulate enviroment
+def train():
+    global epsilon, epsilon_decay
+    while (website_running):
+        state = getState()
+        total_reward = 0
+        if rand.uniform(0,1)< epsilon:
+            #select a random action from action space
+            #Requires enviromet to be defined
+            action = env.action_space.sample()
+            print("Explore")
+        else:
+            #Selects best action from action space using q table
+            action = numpy.argmax(q_table[state])
+            print("Exploit")
+        #This returns fuction returns new_state,reward and done
+        newState, reward, done, _ = env.step(action)
+        total_reward += reward
+        qValue = qTable[state][action]
+        bestQ = numpy.max(qTable[newState])
+        #Qlearing equation 
+        qTable[state][action] = (1 - learningRate) * qValue + learningRate + (reward + gamma + bestQ)
 
+        state = newState
+    return
 
 if __name__ == "__main__":
+    #Enviroment
+    env = gym.make("Webapp")
     #Assuming grid is 3 by 3 for website
     observation_size = [3,3]
-    #Enviroment
-    env = None
     #Exploration vs exploit value if one will always explore
     epsilon = 1
     #How quickly the epsion value changes
     epsilon_decay = 0.7
     #How drastically it 
-    learning_rate = 0.2
+    learningRate = 0.2
     #Discount factor
     gamma = 0.7
     #Initlises q tabl vaues to zero
-    q_table = numpy.zeros(observation_size)
+    qTable = numpy.zeros(observation_size)
