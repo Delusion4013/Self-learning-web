b39b0edc35246f171cd47de65668cf9cd6f41d8e
lofi-marz <marzipant.e@gmail.com>
2020-12-23 13:30:07 +0000

    Works with downloading file

---
+++ react-website/self-learning-app/src/components/TaskButton.js
@@ -0,0 +1,19 @@
+
+
+/**
+ * A button in the LayoutGrid
+ * @param {Object} props 
+ */
+function TaskButton(props) {
+	let x1 = props.rect.x;
+	let y1 = props.rect.y;
+	let x2 = x1 + props.rect.w;	
+	let y2 = y1 + props.rect.h;	
+	return (
+		<button onClick={props.onClick} className="styled-box task-button" style={{gridColumnStart: x1, gridRowStart: y1, gridColumnEnd: x2, gridRowEnd: y2 }}>
+			{props.children}
+		</button>
+	)
+}
+
+export default TaskButton;
